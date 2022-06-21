bf51f97c9120417cb510989a301686b997200eb6
Josh Shafer <sha18011@byui.edu>
2022-06-21 09:46:10 -0600

    added director

--- Game/Director.cs
+++ Game/Director.cs
@@ -1,12 +1,132 @@
 using System;
+using System.Collections.Generic;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+
+namespace Jumper.Game
 {
-    internal class Program
+    // <summary>
+    // A person who directs the game. 
+    //
+    // The responsibility of a Director is to control the sequence of play.
+    // </summary>
+    public class Director
     {
-        static void Main(string[] args)
+        bool firstPlay = true;
+        bool isPlaying = true;
+        int totalScore = 300;
+        Card card = new Card();
+        bool guessIsHigher = false;
+
+        // <summary>
+        // Constructs a new instance of Director.
+        // </summary>
+        public Director()
         {
-            Console.WriteLine("Hello World!");
+            card.Draw();
+        }
+
+        // <summary>
+        // Starts the game by running the main game loop.
+        // </summary>
+        public void StartGame()
+        {
+            while (isPlaying)
+            {
+                GetInputs();
+                DoUpdates();
+                DoOutputs();
+            }
+        }
+
+        // <summary>
+        // Asks the user if they want to play again and their guess.
+        // </summary>
+        public void GetInputs()
+        {   
+            if (!firstPlay)
+            {
+                // Ask user to play again.
+                Console.Write("Play again? [y/n] ");
+
+                // If the users says no end game.
+                isPlaying = (Console.ReadLine() == "y");
+            }
+            else {
+                firstPlay = false;
+            }
+
+            if (!isPlaying) {
+                return;
+            }
+
+            // Print card name so that user can make a guess.
+            Console.WriteLine($"\nThe card is: {card.name}.");
+
+            // GET guess.
+            Console.Write("Higher or lower? [h/l] ");            
+            guessIsHigher = (Console.ReadLine() == "h");
+        }
+
+        // <summary>
+        // Updates the player's score.
+        // </summary>
+        public void DoUpdates()
+        {   
+            // If game is over end game.
+            if (!isPlaying)
+            {
+                return;
+            }
+            
+            // Create variable to store last card value before changed.
+            int lastValue = card.value;
+
+            // Generate new card value.
+            card.Draw();
+
+            // Find out if guess was correct or not.
+            bool valueIsHigher = (card.value > lastValue);
+
+            // Use an xor to figure out player points. 
+            int points = 0;
+
+            if (valueIsHigher ^ guessIsHigher) {
+
+                // Incorrect guess.
+                points = -75;
+
+            } else {
+                
+                // Correct guess.
+                points = 100;
+            }
+
+            // Add points to total score.
+            totalScore += points;
+            
+        }
+
+        // <summary>
+        // Displays the next card and the score.  
+        // </summary>
+        public void DoOutputs()
+        {   
+            // If game is over end game.
+            if (!isPlaying)
+            {
+                return;
+            }
+
+            // Output to user.
+            Console.WriteLine($"Next card was: {card.name}");
+            Console.WriteLine($"Your score is: {totalScore}");
+
+            // Check end game conditions.
+            isPlaying = (totalScore > 0);
+
+            if (!isPlaying)
+            {
+                Console.WriteLine("Game Over");
+            }
         }
     }
-}
